69203dc5916d9fed53ac4aa7b45aecd7b69677d0
dmg94 <dmg94@pitt.edu>
2016-09-18 16:34:03 -0400

    This is exercise 3 from Chapter 2, NOT chapter 1. This program takes user input to know how many square meters of land they have, and then calculates how many acres you have.

---
+++ Ch1_Ex3/ch1_ex3.py
@@ -0,0 +1,25 @@
+# name: Danielle Grentz
+# email: dmg94@pitt.edu
+# date: September 15th, 2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+# TA: Alex Rowden (alex.rowden@pitt.edu
+# Note: This is Chapter 2, exercise 3, NOT chapter 1.
+# This program asks the user to enter a value /
+# of sqaure meters of land and then /
+# calculates the numbers of acres.
+# First, I am making an assignment statement of land to user input
+land = float(input('Please enter the total square meters of land you have.'))
+# Now I am assigning acre to the equivalent amount of square meters
+acre = 4046.8564224
+# Lastly, I am printing out the results for the user to read.
+print( land / acre, 'is how many acres you have')
+
+
+
+
+
+
+
+
+
