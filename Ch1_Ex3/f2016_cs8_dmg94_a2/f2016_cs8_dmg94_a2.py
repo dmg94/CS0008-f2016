07ec40293a6d1a70f3bd70b27da872f98a94ba83
dmg94 <dmg94@pitt.edu>
2016-10-28 19:13:45 -0400

    New folder with 3 files, a run text file, readme text file, and the python program This is assignment 2, which takes a file from the user and adds up 2 total for them, a running total and a line total.

--- Ch1_Ex3/f2016_cs8_dmg94_a2/f2016_cs8_dmg94_a2.py
+++ Ch1_Ex3/f2016_cs8_dmg94_a2/f2016_cs8_dmg94_a2.py
@@ -8,35 +8,31 @@
 # the distance ran by all of the participants in the file as well as how many lines (people) there
 # are in the file as the output by using an indefinite loop until the user puts in 'q' 'quit' or an empty
 # string at the end.
-ptotal_line = 0
-ptotal_dist = 0
+partial_total_line = 0
+partial_total_dist = 0
+total_lines = 0
+total_distance = 0
 def processFile(fh):
 # fh is the variable that is the file handler/object.
-    fh = input('Please enter the path of the first file.' )
+    fh = input('Please provide the file name of the first file.' )
     file_object = open ( fh, 'r')
-
-
     while fh and fh != 'quit' and fh != 'q'
        for line in file_object:
-            ptotal_line += 1
+            partial_total_line += 1
             line = line.rstrip('/n')
             temporary = line.split(',')
             distance = float(temporary[1])
-            ptotal_dist += distance
-            Real_total_dist += ptotal_dist
-            Real_total_line += ptotal_line
-            total_lines += ptotal_line
-            total_dist += ptotal_dist
-    print('File to be read:', file_object)
-    print('Partial # of lines:', ptotal_line)
-    print('Partial distance run', ptotal_dist)
-    file = input('Enter the next file path or press q or quit to end.')
-    printkv('Partial total lines', ptotal_line)
-    printkv('Partial total distance', ptotal_dist)
+            partial_total_dist += distance
+            total_lines += partial_total_line
+            total_dist += partial_total_dist
+        printkv('File to be read', file_object)
+        printkv('Partial # of lines', ptotal_line)
+        printkv('Partial distance run', ptotal_dist)
+        file = input('Please provide the next file name or enter "q" or "quit" to end.')
+        printkv('Total # of lines', total_line)
+        printkv('Total distance run', total_dist )
 # I assigned file_object to be the variable that will work with the file
-    total_lines = 0
-    total_distance = 0
-def printkv(key,value,klen=0)
+  def printkv(key,value,klen=0)
     kl = max(len(key),klen))
     if isinstance (value,str):
         FS = '.30s'
@@ -44,7 +40,7 @@ def printkv(key,value,klen=0)
         FS = '10.3f'
     elif isinstance (value, int):
         FS = '10d'
-    print(key, value(format(value, FS)))
+    print(key(format(key, FS)), value(format(value, FS)))
 
 
 
