52f736b33228aa5c7b2f74f9290e03e1fe9186e7
dmg94 <dmg94@pitt.edu>
2016-10-29 15:02:29 -0400

    New folder with 3 files, a run text file, readme text file, and the python program This is assignment 2, which takes a file from the user and adds up 2 total for them, a running total and a line total.

--- Ch1_Ex3/f2016_cs8_dmg94_a2/f2016_cs8_dmg94_a2.py
+++ Ch1_Ex3/f2016_cs8_dmg94_a2/f2016_cs8_dmg94_a2.py
@@ -8,7 +8,7 @@
 # the distance ran by all of the participants in the file as well as how many lines (people) there
 # are in the file as the output by using an indefinite loop until the user puts in 'q' 'quit' or an empty
 # string at the end.
-# Here I define all the variables that will be used in the loops of the processFile function
+# Here I define all the variables that will be used in the functions I am creating.
 # They need to start at zero so the partial and Total totals add up correctly.
 total_lines = 0
 total_distance = 0
@@ -32,16 +32,17 @@ def printkv(key, value):
     elif isinstance(value, int) or (key, str):
         fs = '10d'
         # Here is how I tell Python to print the process_file(fh) function's table.
-    print(key(format(key, fs)), value(format(value, fs)), ':')
-# Here I am defining the processFile function that I am using.
+    print(key(format(key, fs)), ":", value(format(value, fs)))
+# Here I am defining the process_file function that I am using.
 
 
 def process_file():
-    # fh is the variable that is the file handler/object.
+    # Here I define the global variables used for this function.
     global total_lines
     global total_distance
     global partial_total_line
     global partial_total_dist
+    # fh is the variable that is the file handler/object.
     fh = input('Please enter the first file to be read.')
     # Here I am opening the file object provided by the user
     file_object = open(fh, 'r')
@@ -58,14 +59,16 @@ def process_file():
             partial_total_dist += distance
             total_lines += partial_total_line
             total_distance += partial_total_dist
-            # Printkv function is located above.
+            # Printkv function is located above
+            # Here I am printing the file information that was 
+            # computed in the for loop.
             printkv('File to be read', file_object)
             printkv('Partial # of lines', partial_total_line)
             printkv('Partial distance run', partial_total_dist)
-# Here is where I use the while loop to print out the table for the user.
-# Now I need to have to close the file before I get the next file (or quit).
+        # Now I need to have to close the file before I get the next file (or quit).
         file_object.close()
         fh = input('Please provide the next file name or enter "q" or "quit" to end.')
-# Once the program leaves the while loop, the totals need printed.
+        # Once the program leaves the while loop, the totals need printed.
     printkv('Total number of lines', total_lines)
     printkv('Total distance run', total_distance)
+    return fh
